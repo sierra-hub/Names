54361b6cf5aa3f13fc466cf0dad65f38a09a0012
sierra-hub <kteta.sierra@gmail.com>
2020-10-02 09:29:44 +0200

    added a function to store the days

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $("#myBirthDate").mask("99/99/9999");
 });
+function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
