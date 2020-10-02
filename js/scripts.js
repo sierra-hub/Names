2fb23e7c483a365650a40cb109cf3dba4e727082
sierra-hub <kteta.sierra@gmail.com>
2020-10-02 09:37:12 +0200

    added a function to store male and female names

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $("#myBirthDate").mask("99/99/9999");
 });
-function getAkanName(){
+function getName(){
     var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    var myBirthday = document.getElementById("myBirthDate").value;
+    var myGender = document.getElementsByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
